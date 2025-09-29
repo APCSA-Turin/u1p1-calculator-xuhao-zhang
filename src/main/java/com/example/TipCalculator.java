cdabefda5d25dc122b7bd46ce82a6d4693a59b63
xuhao-zhang <xuhaoz@nycstudents.net>
2025-09-29 16:56:35 +0000

    commit message

--- src/main/java/com/example/TipCalculator.java
+++ src/main/java/com/example/TipCalculator.java
@@ -5,15 +5,35 @@ public class TipCalculator {
     //WRITE YOUR PROGRAM IN calculateTip
     public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
         //DO NOT DELETE ANY OF THE CODE BELOW      
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter cost: ");
+    cost = scan.nextDouble();
+    scan.nextLine();
+    System.out.println("Enter percent for tip in integer form:");
+    percent = scan.nextInt();
+    scan.nextLine();
+    System.out.println("Enter number of people: ");
+    people = scan.nextInt();
+    scan.nextLine();
+
+    double tip = (Math.round((percent / 100 * cost) * 100)) / 100; //I figured out how to round from https://www.geeksforgeeks.org/java/java-math-round-method/
+    double totalBillWithTip = (Math.round((tip + cost) * 100)) / 100;
+    double perPersonCostBeforeTip = (Math.round((cost / people) * 100) / 100);
+    double tipPerPerson = tip / people;
+    double totalCostPerPerson = perPersonCostBeforeTip + tipPerPerson;
+
+    
+    scan.close();
+    
 
         String result = "-------------------------------\n" +
-                       "Total bill before tip: $" + "" + "\n" +
-                       "Total percentage: " + "" + "%\n" +
-                       "Total tip: $" + "" + "\n" +
-                       "Total Bill with tip: $" + "" + "\n" +
-                       "Per person cost before tip: $" + "" + "\n" +
-                       "Tip per person: $" + "" + "\n" +
-                       "Total cost per person: $" + "" + "\n" +
+                       "Total bill before tip: $" + cost + "\n" +
+                       "Total percentage: " + percent + "%\n" +
+                       "Total tip: $" + tip + "\n" +
+                       "Total Bill with tip: $" + totalBillWithTip + "\n" +
+                       "Per person cost before tip: $" + perPersonCostBeforeTip + "\n" +
+                       "Tip per person: $" + tipPerPerson + "\n" +
+                       "Total cost per person: $" + totalCostPerPerson + "\n" +
                        "-------------------------------\n";
 
         return result;
@@ -24,7 +44,7 @@ public class TipCalculator {
         boolean condition = true;
     
         //  COPY AND PASTE YOUR PROGRAM FROM calculateTip() HERE 
-        
+
         // the while loop condition is checked,
         // and if TRUE, runs the code inside.
         // when the code inside is done running, the condition is rechecked,
